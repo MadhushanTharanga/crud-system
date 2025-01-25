cd4be96c8c73232dbc897e9151bac3a5296caf99
MadhushanTharanga <madhushan0617@gmail.com>
2025-01-25 10:56:07 +0530

    updated!

--- customer/customer.js
+++ customer/customer.js
@@ -21,4 +21,106 @@ function validateForm(){
     }else if (age<1){
         alert("Age must not be zero or less than zero");
     }
+
+    const customer = new Customer(Math.floor(Math.random()*10),name,address,email,age);
+    addCustomer(customer);
+    loadTable();
+    const clear=()=>{
+        name.value = '';
+        address.value = '';
+        name.value = '';
+        name.value = '';
+    }
+
+}
+
+
+
+class Customer{
+    #id;
+    #name;
+    #address;
+    #email;
+    #age;
+
+    constructor(id,name,address,email,age){
+        this.#id=id;
+        this.#name=name;
+        this.#address=address;
+        this.#email=email;
+        this.#age=age;
+    }
+    getCustomerId(){
+        return this.#id;
+    }
+    getCustomerName(){
+        return this.#name;
+    }
+    getCustomerAddress(){
+        return this.#address;
+    }
+    getCustomerEmail(){
+        return this.#email;
+    }
+    getCustomerAge(){
+        return this.#age;
+    }
+
+}
+let customerDatabase = [];
+
+function addCustomer(customer){
+    customerDatabase.push(customer);
+}
+function getAllCustomer(){
+    return customerDatabase;
+}
+
+function loadTable(){
+    let tablebody = document.getElementById('table-body');
+    tablebody.innerHTML = '';
+
+    for (const temp of customerDatabase){
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
+            <td>
+                <div class="t-outer">
+                    ${temp.getCustomerId()}
+                </div>
+            </td>
+            <td>
+                <div class="t-outer">
+                    ${temp.getCustomerName()}
+                </div>
+            </td>
+            <td>
+                <div class="t-outer">
+                    ${temp.getCustomerAddress()}
+                </div>
+            </td>
+            <td>
+                <div class="t-outer">
+                    ${temp.getCustomerEmail()}
+                </div>
+            </td>
+            <td>
+                <div class="t-outer">
+                    ${temp.getCustomerAge()}
+                </div>
+            </td>
+            <td>
+                <div class="t-outer">
+                    <input type="button" onclick="readyToUpdate('${temp.getCustomerId()}')" value="Modify" class="btn btn-success">
+                </div>
+            </td>
+            <td>
+                <div class="t-outer">
+                    <input type="button" onclick="deleteCustomer('${temp.getCustomerId()}')"" class="btn btn-danger" value="Remove">
+                </div>
+            </td>
+            
+        `;
+        tablebody.appendChild(tr);
+    }
 }
